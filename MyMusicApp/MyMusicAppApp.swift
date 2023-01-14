fb7ca8e37a00d7882a6cc5a774d8425407e2cb10
ar20190114 <skdr0724@gmail.com>
2023-01-14 17:06:55 +0900

    Initial Commit

---
+++ MyMusicApp/MyMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicAppApp.swift
+//  MyMusicApp
+//
+//  Created by ryotaban on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
